116876ae22d766a0169a31480eb6c1101a6bff50
Shadowind201 <enzocarnier@gmail.com>
2024-05-27 22:21:24 -0300

    atividade

--- somar.js
+++ somar.js
@@ -1,4 +1,7 @@
 function somar(a, b){
+    if((typeof a != 'number') || (typeof b != 'number')){
+        return"Caracter inválido"
+    }
     return a + b
 }
 
--- somar.test.js
+++ somar.test.js
@@ -1,10 +1,35 @@
 //somar.test.js
+const { default: expect } = require("expect")
 const somar  = require("./somar")
 
-test('somar 1 + 2 é igual a 3', ()=>{
-    expect(somar(1,2)).toBe(3)
+test('Soma de dois numero inteiros positivos', ()=>{
+    expect(somar(3,5)).toBe(8)
 })
 
-test('Soma de dois numero inteiros', ()=>{
-    expect(somar(3,5)).ToBe(8)
+test('Soma de dois numero inteiros negativos', ()=>{
+    expect(somar(-3,-5)).toBe(-8)
+})
+
+test('Soma de um número positivo e um número negativo', ()=>{
+    expect(somar(7,-2)).toBe(5)
+})
+
+test('Soma de zero com outro número', ()=>{
+    expect(somar(0,9)).toBe(9)
+})
+
+test('Soma de dois números decimais', ()=>{
+    expect(somar(2.5,3.1)).toBe(5.6)
+})
+
+test('Soma de um número inteiro e um número decimal', ()=>{
+    expect(somar(4,2.5)).toBe(6.5)
+})
+
+test('Verificação de entrada inválida', ()=>{
+    expect(somar("a",3)).toBe("Caracter inválido")
+})
+
+test('Soma de grandes números', ()=>{
+    expect
 })
